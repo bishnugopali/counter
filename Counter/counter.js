26ec578ff321904ed66a094dd4db9256813abd42
bishnugopali <bishnugopali@gmail.com>
2021-12-05 11:13:37 +0000

    Update counter.js
    local storage added

--- Counter/counter.js
+++ Counter/counter.js
@@ -1,26 +1,18 @@
 
-let count=203800000000;
 
+let count=203800000000;
+//set local variable in JS
+//localStorage.setItem('count', count);
+//get local variable set before
+//console.log(localStorage.getItem('count'));
 
 let counterDisplay=document.getElementById('counter');
 
 let countUp=733;
 //increase count each second
+
 setInterval(function(){
-    count=count+countUp;
+    count=parseInt(localStorage.getItem('count'))+countUp;
+    localStorage.setItem('count', count);
     counterDisplay.innerHTML=count.toLocaleString("en-US");
-
 },1000);
-
-
-
-
-
-
-
-
-
-
-    
-
-
